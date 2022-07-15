986823f0ea541c20d3f2f01b6c76dcf03c838064
Nicholas Tavares <48931095+NicholasTavares@users.noreply.github.com>
2022-07-15 11:43:38 -0300

    Create linked_list_push_back.h

---
+++ lista_duplamente_encadeada/linked_list_push_back.h
@@ -0,0 +1,13 @@
+#ifndef __LINKED_LIST_PUSH_BACK__
+#define __LINKED_LIST_PUSH_BACK__
+#include "linked_list_int.h"
+
+/*
+    Insere um elemento no final da lista duplamente ligada.
+        *list = ponteiro para a lista duplamente ligada.
+        i = inteiro a ser inserido no final da lista duplamente ligada.
+*/
+
+unsigned int linked_list_push_back(linked_list_int * list, int i);
+
+#endif
