9c3093c9d7dcf4925e71362c770cf6baf9925601
Nicholas Tavares <48931095+NicholasTavares@users.noreply.github.com>
2022-07-15 11:42:12 -0300

    Update linked_list_create.c

--- lista_duplamente_encadeada/linked_list_create.c
+++ lista_duplamente_encadeada/linked_list_create.c
@@ -6,14 +6,8 @@ struct linked_list_int *linked_list_create() {
   struct linked_list_int *new_liked_list =
       (struct linked_list_int *)malloc(sizeof(struct linked_list_int));
 
-  new_liked_list->first =
-      (struct list_int_node *)malloc(sizeof(struct list_int_node));
-
-  new_liked_list->first->next = new_liked_list->first;
-  new_liked_list->first->prev = new_liked_list->first;
-
-  new_liked_list->last = new_liked_list->first;
-
+  new_liked_list->first = NULL;
+  new_liked_list->last = NULL;
   new_liked_list->size = 0;
 
   return new_liked_list;
