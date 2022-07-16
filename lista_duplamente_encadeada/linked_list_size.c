0c873f7e2d8b527f44dedaf9173cb6d54728e2b5
NicholasTavares <nicholasbalby@hotmail.com>
2022-07-16 19:52:22 -0300

    feat: retorna o tamanho da lista duplamente ligada

---
+++ lista_duplamente_encadeada/linked_list_size.c
@@ -0,0 +1,5 @@
+#include "linked_list_size.h"
+
+unsigned int linked_list_size(struct linked_list_int* list) {
+  return list->size;
+}
---
+++ lista_duplamente_encadeada/linked_list_size.h
@@ -0,0 +1,12 @@
+#ifndef __LINKED__LIST__SIZE__
+#define __LINKED__LIST__SIZE__
+#include "linked_list_int.h"
+
+/*
+  Retorna o tamanho atual da lista.
+    *list = ponteiro para a lista ligada.
+*/
+
+unsigned int linked_list_size(struct linked_list_int* list);
+
+#endif
